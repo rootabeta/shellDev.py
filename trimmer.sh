d94cda4d0340b23e076063303f1f0d91d51484fc
rootabeta <rootabeta@protonmail.com>
2022-04-21 17:42:07 -0400

    Fixed trimmer

--- trimmer.sh
+++ trimmer.sh
@@ -1,3 +1,3 @@
-cat shellcode.txt | sed -s 's/0x/\\x/g' | sed -s "s/, //g" | tr -d '\n' ; echo
+cat $1 | grep -v "shellcode" | sed -s 's/0x/\\x/g' | sed -s "s/, //g" | tr -d '\n' | tr -d "}" | tr -d ';' ; echo
 #Takes 0x00, 0x01, etc. output (no char shellcode[] = ... or unsigned int = ... etc) and turns it into shellcode
 #Good for making exploits
